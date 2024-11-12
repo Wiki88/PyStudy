b77c24419c62a6a11482d686ac5b19c048f2ab3b
Wiki88 <doroshenkoione@gmail.com>
2024-11-12 11:40:09 +0300

    Update module_2_hard_v2.py

--- module_2_hard_v2.py
+++ module_2_hard_v2.py
@@ -5,18 +5,15 @@ def cipher():
     inset = random.choice(n)
     return inset
 
-inset1 = cipher()
-
 def gen_pass(n):
     result = ""
     for i in range(1,21):
         for j in range(i+1, 21):
-            sum = i + j
-            if n % sum == 0:
+            sum_ = i + j
+            if n % sum_ == 0:
                 result+= f'{i}{j}'
     return result
-n = inset1
+n = cipher()
 if 3 <= n <= 20:
     set = gen_pass(n)
     print('Случайное число' , n , 'Пароль', set)
-
