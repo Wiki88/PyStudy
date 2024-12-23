313832bb6f2393504ac40c380ae894b36a132981
Wiki88 <doroshenkoione@gmail.com>
2024-12-23 17:36:32 +0300

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -3,7 +3,7 @@
 def get_multiplied_digits(number):
     str_number = str(number)
     first_ = int(str_number[0])
-    if len(str_number) > 1:
+    if len(str_number) <= 1:
         return first_
     else:
         return first_ * get_multiplied_digits(int(str_number[1:]))
