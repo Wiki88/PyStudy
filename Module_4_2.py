6e973508a131aad9e89806dad69ed8f5000e2760
Wiki88 <doroshenkoione@gmail.com>
2025-01-11 21:14:25 +0300

    Add files via upload

---
+++ Module_4_2.py
@@ -0,0 +1,11 @@
+#Пространство имён. Namespace
+
+def test_function():
+	def inner_function():
+		print('Я в области видимости функции test_function')
+	inner_function()
+	return
+
+test_function()
+
+#inner_function() # NameError: name 'inner_function' is not defined.
